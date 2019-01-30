29c8ce80fc36ab38a27ed6f80827a4cc38147a61
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-29 19:22:48 -0500

    Added website list to be blocked

--- WebBlocker.py
+++ WebBlocker.py
@@ -5,3 +5,7 @@ hostPath = r"C:\Windows\System32\drivers\etc"
 
 # The IP the program redirects the web browser for the blocked websites
 redirect = "127.0.0.1"
+
+# Website list to be blocked
+website_list = ["www.facebook.com", "facebook.com", "www.google.com", "google.com"]
+
