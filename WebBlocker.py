075320314426b0039360b6fe700ea23d0bf61942
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-29 20:29:55 -0500

    Added sleep time in loop

--- WebBlocker.py
+++ WebBlocker.py
@@ -1,5 +1,8 @@
 """ Website Blocker """
 
+from datetime import datetime as dt
+import time
+
 # Adding the host path
 hostPath = r"C:\Windows\System32\drivers\etc\HOSTS"
 
@@ -9,3 +12,9 @@ redirect = "127.0.0.1"
 # Website list to be blocked
 website_list = ["www.facebook.com", "facebook.com", "www.google.com", "google.com"]
 
+while True:
+    # Check whether the current time is between working hours 8-16
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
+        print("Working hours ... WebBlock is running")
+    time.sleep(5)
+        
