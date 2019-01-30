ce9c94630cacb594901d4db08ec128b287d0b9d1
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-29 22:04:29 -0500

    Lines in hostpath were added w/o websites

--- WebBlocker.py
+++ WebBlocker.py
@@ -4,7 +4,8 @@ from datetime import datetime as dt
 import time
 
 # Adding the host path
-hostPath = r"C:\Windows\System32\drivers\etc\HOSTS"
+hostPath = r"D:\DataScience\Python\PythonUdemyArditSulce\Python10Apps\WebBlocker\WebBlocker\HOSTS"
+# hostPath = r"C:\Windows\System32\drivers\etc\HOSTS"
 
 # The IP the program redirects the web browser for the blocked websites
 redirect = "127.0.0.1"
@@ -14,7 +15,7 @@ websiteList = ["www.facebook.com", "facebook.com", "www.google.com", "google.com
 
 while True:
     # Check whether the current time is between working hours 8-16
-    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,22):
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,21):
         print("Working hours ... WebBlock is running")
         with open(hostPath, 'r+') as file:
             content = file.read()
@@ -23,5 +24,11 @@ while True:
                     pass
                 else:
                     file.write(redirect + " " + website + "\n")
-    time.sleep(5)
+    else:
+        with open(hostPath, 'r+') as file:
+            content = file.readlines()
+            for line in content:
+                if not any(website in line for website in websiteList):
+                    file.write(line)
+    time.sleep(1000)
         
